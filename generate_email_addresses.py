3939d217f9703681d4eaaff0676742e9086c7ebc
Nick-Phaita <nickphaita@gmail.com>
2024-09-05 00:43:20 +0300

    Initial commit

---
+++ generate_email_addresses.py
@@ -0,0 +1,8 @@
+"""This python module generates email addresses based on the following specifications:
+- mail addresses should be unique
+- use two names if available
+- no special characters in an email address
+"""
+from typing import List
+
+#def generate_email_addresses(file_path: str) -> List[str]:
